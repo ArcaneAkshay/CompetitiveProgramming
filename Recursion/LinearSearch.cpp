b8b1a06c5fd92dcf0dd841b8564427d7333b76ea
Akshay <akshaymalik20112000@gmail.com>
2020-09-27 02:06:08 +0530

    recursion updated

--- Recursion/LinearSearch.cpp
+++ Recursion/LinearSearch.cpp
@@ -1,18 +1,60 @@
 #include<iostream>
 using namespace std;
 
-int linearSearch(int arr[], int n, int key)
+// Method 1 --> Same as finding the last occurence of an element
+int linearSearch_1(int arr[], int n, int key)
+{
+	if(n == 0)
+		return -1;
+	int i = linearSearch_1(arr +1, n - 1,key);
+	if(i == -1)
+	{
+		if(arr[0] == key)
+			return 0;
+		return -1;
+	}
+	// if i returned by the subproblem is not -1 that means it is present at 0th
+	// index in the subproblem meaning it is present at 1st index of the given problem scope
+	// hence we are adding 1 every time if we find the element in the subproblem
+	return i + 1;
+}
+
+// Method 2 --> Same as finding the last occurence of an element
+int linearSearch_2(int arr[], int n, int key)
 {
 	if(n==-1)
 		return -1;
 	if(arr[n] == key)
 		return n;
-	return linearSearch(arr, --n, key);
+	return linearSearch_2(arr, --n, key);
+}
+
+// Method 3 --> Same as finding the first occurence of an element
+int linearSearch_3(int arr[], int i, int n, int key)
+{
+	if(i == n)
+		return -1;
+	if(arr[i] == key)
+		return i;
+	return linearSearch_3(arr, i + 1, n, key);
+}
+
+// Method 4 --> Same as finding the first occurence of an element
+int linearSearch_4(int arr[], int n, int key)
+{
+	if(n == 0)
+		return -1;
+	if(arr[0] == key)
+		return 0;
+	int i = linearSearch_4(arr + 1, n - 1, key);
+	if(i==-1)
+		return -1;
+	return i+1;
 }
 
 int main()
 {
-	int arr[5] = {1,2,5,4,3};
-	cout<<linearSearch(arr, 5, 5);
+	int arr[6] = {1,4,2,4,4,3};
+	cout<<linearSearch_1(arr, 6, 4);
 	return 0;
 }
