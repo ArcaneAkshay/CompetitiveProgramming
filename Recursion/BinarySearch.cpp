3f07c920767d81aba8f5718ed89512a8f2958c13
Akshay <akshaymalik20112000@gmail.com>
2020-09-30 11:49:39 +0530

    binarySearch.cpp updated

--- Recursion/BinarySearch.cpp
+++ Recursion/BinarySearch.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 using namespace std;
 
+// Finding the element in the array given that it is unique
 template<typename T>
-T binarySearch(T arr[], int l, int r, int key)
+T binarySearch_1(T arr[], int l, int r, int key)
 {
 	if(l<=r)
 	{
@@ -10,18 +11,67 @@ T binarySearch(T arr[], int l, int r, int key)
 		if(arr[mid] == key)
 			return mid;
 		if(arr[mid] > key)
-			return binarySearch(arr,l,mid - 1,key);
-		return binarySearch(arr,mid+1, r,key);
+			return binarySearch_1(arr,l,mid - 1,key);
+		return binarySearch_1(arr,mid+1, r,key);
 	}
 	return -1;
 
 }
 
+// Given the element is not unique (duplpicates are present in the array)
+
+// Finding the first occurence of the element
+template<typename T>
+T binarySearch_2(T arr[], int l, int r, int key)
+{
+	if(l<=r)
+	{
+		int mid =  l + (r - l)/2;
+		if(arr[mid] == key)
+		{
+			if(mid == 0) // or if(l == r) return l;
+				return 0;
+			if(arr[mid-1] < arr[mid])
+				return mid;
+			if(arr[mid-1] == key)
+				return binarySearch_2(arr,l,mid - 1,key);
+		}
+		if(arr[mid] > key)
+			return binarySearch_2(arr,l,mid - 1,key);
+		return binarySearch_2(arr,mid+1, r,key);
+	}
+	return -1;
+}
+
+
+// Finding the last occurence of the element
+template<typename T>
+T binarySearch_3(T arr[], int l, int r, int key)
+{
+	if(l<=r)
+	{
+		int mid =  l + (r - l)/2;
+		if(arr[mid] == key)
+		{
+			if(l == r)
+				return mid;
+			if(arr[mid] < arr[mid + 1])
+				return mid;
+			if(arr[mid + 1] == key)
+				return binarySearch_3(arr, mid + 1, r, key);
+		}
+		if(arr[mid] > key)
+			return binarySearch_3(arr,l,mid - 1,key);
+		return binarySearch_3(arr,mid+1, r,key);
+	}
+	return -1;
+}
+
 int main()
 {
-	int arr[5] = {1,2,3,4,5};
-	int key = 8;
-	int i = binarySearch(arr,0,5,key);
+	int arr[5] = {1,1,3,5,5};
+	int key = 5;
+	int i = binarySearch_1(arr,0,5,key);
 	if(i!=-1)
 		cout<<i<<endl;
 	else
